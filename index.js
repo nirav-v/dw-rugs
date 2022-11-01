f53f1a4e7791db8cef8893bc48837b33d6947b0a
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-11-01 12:00:20 -0700

    gallery as flex box of images with no stretch or shrink

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var rugGrid = document.querySelector(".rug-grid");
 for (pic of rugPics) {
   // console.log(pic)
   rugGrid.innerHTML+=(`  
+ 
     <img src = '../images/DW Rugs Photo Gallery/${pic}' alt='rug image' class='rug-pic'/>
+
           `);
 }
