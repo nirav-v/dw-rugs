2edce7fc1c535434d969d3403a742ee07aed950b
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-11-16 09:38:10 -0800

    added video descriptions, comment explaining how I access image file names and append them inside inner html of rug-grid. Rug grid inner html now has div containing each image, and css media query requiring 50% with when under 500px

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ var rugGrid = document.querySelector(".rug-grid");
 
 for (pic of rugPics) {
   // console.log(pic)
-  rugGrid.innerHTML+=(`  
- 
+  rugGrid.innerHTML += `  
+ <div class=rug-container>
     <img src = '../images/DW Rugs Photo Gallery/${pic}' alt='rug image' class='rug-pic'/>
+  </div>
 
-          `);
+          `;
 }
--- utils/readrugs.js
+++ utils/readrugs.js
@@ -1,6 +1,4 @@
-import { readdir } from 'node:fs/promises';
-
-
+import { readdir } from "node:fs/promises";
 
 var rugPics = [];
 
@@ -8,16 +6,19 @@ async function getRugs(path, arr) {
   try {
     const files = await readdir(path);
     for (const file of files) {
-        // console.log(file);
-        arr.push(file);
-    };
-    console.log(arr)
-    return arr
+      // console.log(file);
+      arr.push(file);
+    }
+    console.log(arr);
+    return arr;
   } catch (err) {
     console.error(err);
   }
 }
 
-getRugs('./images/DW Rugs Photo Gallery', rugPics);
+getRugs("./images/DW Rugs Photo Gallery", rugPics);
 
-console.log(rugPics)
+// kind of jank way to do it, but I am basically console logging an array of all the image file names in an images folder and then pasting that array content inside a rugPics js variable (see rugPics.js for images array).
+// I then query select the empty rug-grid div in the gallery page. Then, for eah image file in the rugPics array, I create a new image html tag with the image name as its src attribute and append that image to the rug grid div.
+
+console.log(rugPics);
